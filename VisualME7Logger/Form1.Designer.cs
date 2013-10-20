dac0fb99073078409d89ef8cb3ec4b64be6ec62d
sbloom82_cp <SND\sbloom82_cp>
2013-10-20 16:37:14 +0000

    Added some basic charts to example form.

--- VisualME7Logger/Form1.Designer.cs
+++ VisualME7Logger/Form1.Designer.cs
@@ -28,6 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtTimestamp = new System.Windows.Forms.TextBox();
@@ -36,6 +41,10 @@
             this.txtNames = new System.Windows.Forms.TextBox();
             this.txtValues = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -51,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(119, 15);
+            this.label1.Location = new System.Drawing.Point(147, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 1;
@@ -59,7 +68,7 @@
             // 
             // txtTimestamp
             // 
-            this.txtTimestamp.Location = new System.Drawing.Point(186, 12);
+            this.txtTimestamp.Location = new System.Drawing.Point(214, 14);
             this.txtTimestamp.Name = "txtTimestamp";
             this.txtTimestamp.Size = new System.Drawing.Size(100, 20);
             this.txtTimestamp.TabIndex = 2;
@@ -76,7 +85,7 @@
             // 
             // txtRunningTime
             // 
-            this.txtRunningTime.Location = new System.Drawing.Point(186, 36);
+            this.txtRunningTime.Location = new System.Drawing.Point(214, 38);
             this.txtRunningTime.Name = "txtRunningTime";
             this.txtRunningTime.Size = new System.Drawing.Size(100, 20);
             this.txtRunningTime.TabIndex = 22;
@@ -86,32 +95,63 @@
             this.txtNames.Location = new System.Drawing.Point(12, 67);
             this.txtNames.Multiline = true;
             this.txtNames.Name = "txtNames";
-            this.txtNames.Size = new System.Drawing.Size(134, 300);
+            this.txtNames.Size = new System.Drawing.Size(204, 300);
             this.txtNames.TabIndex = 23;
             this.txtNames.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // txtValues
             // 
-            this.txtValues.Location = new System.Drawing.Point(152, 67);
+            this.txtValues.Location = new System.Drawing.Point(222, 67);
             this.txtValues.Multiline = true;
             this.txtValues.Name = "txtValues";
-            this.txtValues.Size = new System.Drawing.Size(134, 300);
+            this.txtValues.Size = new System.Drawing.Size(92, 300);
             this.txtValues.TabIndex = 24;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(126, 40);
+            this.label2.Location = new System.Drawing.Point(154, 42);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 25;
             this.label2.Text = "Real time:";
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(320, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(455, 355);
+            this.chart1.TabIndex = 26;
+            this.chart1.Text = "chart";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(12, 372);
+            this.chart2.Name = "chart2";
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chart2.Series.Add(series1);
+            this.chart2.Size = new System.Drawing.Size(302, 176);
+            this.chart2.TabIndex = 27;
+            this.chart2.Text = "chart2";
+            title1.Name = "Title1";
+            title1.Text = "RPM";
+            this.chart2.Titles.Add(title1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(298, 378);
+            this.ClientSize = new System.Drawing.Size(787, 560);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtValues);
             this.Controls.Add(this.txtNames);
@@ -122,6 +162,9 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,6 +180,8 @@
         private System.Windows.Forms.TextBox txtNames;
         private System.Windows.Forms.TextBox txtValues;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
     }
 }
 
