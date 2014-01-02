28d2a49e02831abc83d05e905125803cfd677691
sbloom82_cp <sbloom82@users.noreply.github.com>
2014-01-02 02:02:36 +0000

    mark for add

---
+++ VisualME7Logger.Output/ChecksumInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Diagnostics;
+
+namespace VisualME7Logger.Output
+{
+    public class ChecksumInfo
+    {
+        public string ApplicationPath { get; private set; }
+        public string BinPath { get; private set; }
+
+        public void Read(XElement element)
+        {
+            foreach (XAttribute att in element.Attributes())
+            {
+                switch (att.Name.LocalName)
+                {
+                    case "ApplicationPath":
+                        this.ApplicationPath = att.Value;
+                        break;
+                    case "BinPath":
+                        this.BinPath = att.Value;
+                        break;
+                }
+            }
+        }
+
+        public XElement Write()
+        {
+            XElement ele = new XElement("ChecksumInfo");
+            ele.Add(new XAttribute("ApplicationPath", this.ApplicationPath));
+            ele.Add(new XAttribute("BinPath", this.BinPath));
+            return ele;
+        }
+
+        public ChecksumResult Check()
+        {
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo(
+                this.ApplicationPath,
+                this.BinPath);
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            if (p.Start())
+            {
+                string output = p.StandardOutput.ReadToEnd();
+                string error = p.StandardError.ReadToEnd();
+                return new ChecksumResult() { Success = p.ExitCode == 0, Output = output + "\r\n" + error };
+            }
+            else
+            {
+                return new ChecksumResult() { Success = false, Output = "Unable to start process" };
+            }
+        }
+    }
+
+    public class ChecksumResult
+    {
+        public bool Success { get; set; }
+        public string Output { get; set; }
+    }
+}
